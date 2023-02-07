a4454651f663a100624f9336fa5e6ab225af6cd8
unknown <luccasdrapala@gmail.com>
2023-02-07 20:50:45 -0300

    initial files

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/routes/web.php';
+
+use Pecee\SimpleRouter\SimpleRouter;
+
+SimpleRouter::start();
+
+echo 'hello world';
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'luccas/crud',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'luccas/crud' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'pecee/simple-router' => array(
+            'pretty_version' => '4.3.7.2',
+            'version' => '4.3.7.2',
+            'reference' => '032a2ae7e0e2d876599758f85b61bc965a63ea7c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../pecee/simple-router',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
