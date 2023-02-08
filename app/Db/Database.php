6abbc7c1283445ebedfbede7f9a8b7e03afdab18
unknown <luccasdrapala@gmail.com>
2023-02-07 21:01:18 -0300

    setting database file

---
+++ app/Db/Database.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Db;
+use PDO;
+use PDOException;   
+
+class database {
+
+    //variaveis de conexão com o banco de dados
+    const HOST = 'localhost';
+    const NAME = 'projetinho-store';
+    const USER = 'root';
+    const PASS = '';
+
+    private $table;
+    private $connection;
+
+    public function __construct($table = null) 
+    {
+        $this->table = $table;
+        $this->setConnection();
+    }
+    
+    public function setConnection()
+    {
+        try {
+            $this->connection = new PDO('mysql:host=' . self::HOST . ';dbname=' . self::NAME, self::USER, self::PASS);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die('ERROR CATCH'. $e->getmessage());
+        }
+    }
+}
+
+?>
