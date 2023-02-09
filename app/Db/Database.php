415a5bd0b5cc6488111a0889d0bae44325c4768a
unknown <luccasdrapala@gmail.com>
2023-02-08 21:55:37 -0300

    setting some db methods

--- app/Db/Database.php
+++ app/Db/Database.php
@@ -30,6 +30,46 @@ class Database {
             die('ERROR CATCH'. $e->getmessage());
         }
     }
+
+    public function executeQuery(string $sql) :array
+    {
+        return $this->connection->query($sql)->fetchAll(\PDO::FETCH_CLASS, self::class); // FETCH_CLASS retorna o array
+    }
+
+    public function get(?string $where = '') :array 
+    {   
+        $sql = "SELECT * FROM {$this->table}" . $where;
+        return $this->connection->query($sql)->fetchAll(\PDO::FETCH_CLASS, self::class); // FETCH_CLASS retorna o array
+    }
+
+    public function delete(int $id) :bool 
+    { 
+        $sql = "DELETE FROM {$this->table} WHERE id = :id";
+        $prepare = $this->connection->prepare($sql);
+        $prepare->bindValue(":id", $id);
+
+        return $prepare->execute();
+    }
+
+    public function salesDelete(int $id) :bool 
+    { 
+        $sql = "DELETE FROM {$this->table} WHERE id = :id";
+        $prepare = $this->connection->prepare($sql);
+        $prepare->bindValue(":id", $id);
+
+        return $prepare->execute();
+    }
+
+    public function update(int $id, array $data) :bool
+    {
+        $array_fields = implode("=?", array_keys($data));
+
+        $sql = "UPDATE FROM {$this->table} SET {$array_fields} WHERE id = {$id}";
+        $prepare = $this->connection->prepare($sql);
+        
+        return $prepare->execute(array_values($data));
+    }
+
 }
 
 ?>
