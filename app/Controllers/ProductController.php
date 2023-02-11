ca6419457bad1e7210e07fa54a7ea478f224a5b1
unknown <luccasdrapala@gmail.com>
2023-02-11 20:43:25 -0300

    config select foreach

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -10,12 +10,22 @@ use App\Helpers\View;
 class ProductController extends Controller
 {
     protected ProductModel $productModel;
+    protected ProductTypeModel $productTypeModel;
     private array $data;
 
     public function __construct()
     {
         $this->productModel = new ProductModel();
+        $this->productTypeModel = new ProductTypeModel();
         $this->data['product'] = $this->productModel->get();
+        $this->data['product_type'] = $this->productTypeModel->get();
+    }
+
+    public function index(){
+
+        View::include('includes/header.php');
+        View::include('products/index-products.php', $this->data);
+        View::include('includes/footer.php');
     }
 }
 
--- app/Views/Products/index-products.php
+++ app/Views/Products/index-products.php
@@ -2,7 +2,7 @@
     <hr>
 
     <!-- MODAL TRIGGER -->
-    <button class="btn btn-warning mt-3" data-bs-toggle="modal" data-bs-target="#staticBackdrop">New Product</button>
+    <button type="button"  class="btn btn-warning" data-toggle="modal" data-target="#exampleModal">New Type</button>
 
     <table class="table mt-3 table-secondary border border-dark">
 
@@ -49,8 +49,56 @@
        </tbody> 
     </table>
 
-<!-- Modal -->
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Product Data</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body container text-center">
+        
+      <div class="row">
+        <div class="mt-2 col-12">
+            <label for="productDescription" class="form-label">Product Description</label>
+            <input class="form-control" id="productDescription" type="text">
+        </div>
+      </div>   
+
+      <div class="row">
+        <div class="mt-2 col-6">
+            <label for="productPrice" class="form-label">Product Price</label>
+            <input class="form-control" id="productPrice" type="number" placeholder="0,00">
+        </div>
+
+        <div class="mt-2 col-6">
+
+            <label for="productType" class="">Product Type</label>
+
+            <select id="productType" class="custom-select" name="productType" id="">
+                <option selected value="#">Select a type</option>
+                <?php foreach($product_type as $type): ?>
+                <option value="<?=$type->id?>"><?=$type->product_description?></option>
+                <?php endforeach;?>
+                
+            </select>
+        </div>
+        </div>
+      
+      </div>
+      <div class="modal-footer">
+        <button type="button" onclick="" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" onclick="" class="btn btn-primary">Save changes</button>
+      </div>
+
+    </div>
+    </div>
+    </div>
+
+
+<!-- <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -58,7 +106,6 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body container text-center">
-        <!-- BODY -->
 
             <div class="mt-2 col-12">
                 <label for="productDescription" class="form-label">Product Description</label>
@@ -90,4 +137,4 @@
       </div>
     </div>
   </div>
-</div>
+</div> -->
