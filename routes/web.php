5ea2afad6ee0a0ece568b96c76be8a1a14ace955
unknown <luccasdrapala@gmail.com>
2023-02-11 14:43:20 -0300

    updating routes

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ SimpleRouter::setDefaultNamespace('App\Controllers');
 
 SimpleRouter::get('/product_types', 'ProductTypeController@index');
 SimpleRouter::post('/product_types/create', 'ProductTypeController@createProductType');
+SimpleRouter::post('/product_types/update/{id}', 'ProductTypeController@');
+SimpleRouter::post('/product_types/delete/{id}', 'ProductTypeController@');
+
